2dd55b2f2599bc3fc5c137ff45a7b5ed6dcdd07c
Wim Keirsgieter <wim.keirsgieter@gmail.com>
2016-12-14 16:41:48 +0200

    read version from VERSION

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vumi-telegram',
-    version='0.1.0',
+    version=open('VERSION', 'r').read().strip(),
     url='https://github.com/praekelt/vumi-telegram',
     license='BSD',
     description='A Telegram transport for Vumi and Junebug',
